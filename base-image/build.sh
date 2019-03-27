8ce3a4e46904c4658310c05065f44f94e1fa3c40
Jared Wilkening <jared@mg-rast.org>
2019-03-27 16:21:26 -0500

    improved image tagging, offline build option

--- base-image/build.sh
+++ base-image/build.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-docker build -t mgrast/django-base:latest .
+DOCKERFILE="-f Dockerfile"
+if  [ "$1" = 'offline' ]; then
+    DOCKERFILE='-f Dockerfile-offline'
+    shift
+fi
 
-# cleanup
-#docker images | grep django-base | cut -f4 -d' ' | xargs -I{} echo "mgrast/django-base:"{} | xargs docker rmi
+IMAGE=${1:-'mgrast/django-base'}
+
+echo "Building ${IMAGE} from ${DOCKERFILE}"
+
+docker build -t ${IMAGE}:build ${DOCKERFILE} .
+TAG=`docker images | grep ${IMAGE} | grep build | awk '{print $3}'`
+
+# tag build and del previous 'latest'
+docker tag ${IMAGE}:build ${IMAGE}:${TAG}
+docker rmi ${IMAGE}:latest
+
+# tag new build as latest and delete build
+docker tag ${IMAGE}:${TAG} ${IMAGE}:latest
+docker rmi ${IMAGE}:build
